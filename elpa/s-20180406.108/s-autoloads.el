c372484d98de58bfbbd36220159400c49ffc904f
Peter Coles <peter@mrcoles.com>
2018-06-14 15:06:23 -0400

    updated packages

---
+++ elpa/s-20180406.108/s-autoloads.el
@@ -0,0 +1,15 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("s.el") (23330 48159 562492 836000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; s-autoloads.el ends here
--- elpa/solidity-mode-20171231.741/solidity-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; solidity-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "solidity-mode" "solidity-mode.el" (23160 62264
-;;;;;;  872073 822000))
-;;; Generated autoloads from solidity-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.sol\\'" . solidity-mode))
-
-(autoload 'solidity-mode "solidity-mode" "\
-Major mode for editing solidity language buffers.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("solidity-common.el" "solidity-flycheck.el"
-;;;;;;  "solidity-mode-pkg.el") (23160 62264 870771 363000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; solidity-mode-autoloads.el ends here
