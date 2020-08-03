f9f06f0bcb867ae53796f8cb1d4ab88cea31013d
Peter Coles <peter@mrcoles.com>
2020-08-02 21:43:54 -0400

    error loading melpa?

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -2,14 +2,14 @@
 (require 'dash)
 
 ;; Add melpa to package repos
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; getting error loading this... (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (setq package-pinned-packages '())
 
 (package-initialize)
 
-(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
+(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa-stable")
   (package-refresh-contents))
 
 (defun packages-install (packages)
